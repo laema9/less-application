5ff258e7965a2b46803d6d7b97a0caffe8004ae7
laema9 <laema9@gmail.com>
2025-06-04 15:11:13 +0200

    feat(sentry): integrate Sentry for error tracking and monitoring

--- src/features/home/pages/HomePage.tsx
+++ src/features/home/pages/HomePage.tsx
@@ -1,7 +1,15 @@
 export default function HomePage() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen p-4">
-      <h1 className="text-2xl font-bold">🏠 Homepage</h1>
+    <div className="p-4">
+      <h1 className="text-2xl font-bold">🏠 Home</h1>
+      <button
+        className="mt-4 px-4 py-2 bg-red-500 text-white rounded"
+        onClick={() => {
+          throw new Error("This is your first error!")
+        }}
+      >
+        Break the world
+      </button>
     </div>
   )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,19 @@
-import React from "react"
-import ReactDOM from "react-dom/client"
+import * as ReactDOM from "react-dom/client"
 import App from "./app/App"
+import * as Sentry from "@sentry/react"
 import "./index.css" 
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-)
+
+// Initialisation Sentry
+Sentry.init({
+  dsn: import.meta.env.VITE_SENTRY_DSN,
+  sendDefaultPii: true,
+  integrations: [Sentry.browserTracingIntegration()],
+  tracesSampleRate: 1.0,
+  tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
+})
+
+
+const container = document.getElementById("root")!
+const root = ReactDOM.createRoot(container)
+root.render(<App />)
