fd25407303dcf9665a9c00915a525d2e16cbd875
laema9 <laema9@gmail.com>
2025-06-05 19:15:43 +0200

    feat(homepage): initial homepage layout with ShadcnUI cards integration

--- src/features/home/pages/HomePage.tsx
+++ src/features/home/pages/HomePage.tsx
@@ -1,10 +1,25 @@
+import { CryptoListTable } from "@/features/tools/crypto-list-table/components/CryptoListTable";
+import HomepageCards from "../components/HomepageCards";
 
 export default function HomePage() {
   return (
     <div className="p-4">
-      <header>
-        
-      </header>
+      
+      <div className="flex flex-wrap gap-4">
+        <div className="flex-1 min-w-[200px]">
+          <HomepageCards />
+        </div>
+        <div className="hidden sm:flex flex-1 min-w-[200px]">
+          <HomepageCards />
+        </div>
+        <div className="hidden md:flex flex-1 min-w-[200px]">
+          <HomepageCards />
+      </div>
+    </div>
+
+      <section className="mt-6 ">
+        <CryptoListTable />
+      </section>
     </div>
   )
 }
