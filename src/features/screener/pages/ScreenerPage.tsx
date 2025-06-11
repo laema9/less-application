2c56845e0a4b1c2cfd3e16023d97f081f3bb79c7
laema9 <laema9@gmail.com>
2025-06-11 13:08:58 +0200

    feat(screener): add context-menu component using shadcn/ui

--- src/features/screener/pages/ScreenerPage.tsx
+++ src/features/screener/pages/ScreenerPage.tsx
@@ -1,7 +1,13 @@
+import { ScreenerContextMenu } from "../components/ScreenerContextMenu";
+
 export default function ScreenerPage() {
     return(
-        <div className="p-5">
+        <div className="sm:p-5 p-0 mt-5">
             <p className="text-xl">LESS Screener 0.0.1</p>
+
+            <div className="mt-5">
+                <ScreenerContextMenu />
+            </div>
         </div>
     )
 }
